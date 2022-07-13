c3561a9464c6f6534395c07ec28ad93a3599fdd8
Young998904 <like0998904@gmail.com>
2022-07-13 13:47:02 +0900

    WiseSayings 배열 도입

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,13 +1,17 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     Scanner sc;
     int wiseSayingLastId;
+    List<WiseSaying> wiseSayings;
     public App() {
         sc = new Scanner(System.in);
         wiseSayingLastId = 0;
+        wiseSayings = new ArrayList<>();
     }
     public void run() {
         String cmd;
@@ -29,6 +33,8 @@ public class App {
                     wiseSaying.context = sc.nextLine().trim();
                     System.out.printf("작가 : ");
                     wiseSaying.author = sc.nextLine().trim();
+
+                    wiseSayings.add(wiseSaying);
 
                     System.out.printf("%d번 명언이 등록되었습니다.\n", wiseSayingLastId);
                     break;
